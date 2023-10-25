1c1d10898f679a3ce2b16d674edbe25ba2eac88c
Kaushik Sharma <kaushik445566@gmail.com>
2023-10-25 16:00:10 +0530

    charts added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-import Pagination from './components/Pagination';
+import prisma from '@/prisma/client';
+import IssueSummary from './IssueSummary';
+import LatestIssues from './LatestIssues';
+import IssueChart from './IssueChart';
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  return (
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
-  );
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' },
+  });
+
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
 }
